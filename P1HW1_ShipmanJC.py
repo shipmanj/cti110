c76462aed6bdc20cc3749aaa274dbdaf18a8ceeb
shipmanj <113939725+shipmanj@users.noreply.github.com>
2022-09-21 13:25:10 -0400

    Add files via upload
    First Submission

---
+++ P1HW1_ShipmanJC.py
@@ -0,0 +1,14 @@
+user_num = int(input('Enter integer:\n'))
+user_squared = user_num * user_num
+user_cubed = user_num * user_num * user_num
+
+print ('You entered:', user_num)
+print (user_num, 'squared is', user_squared)
+print ('And', user_num, 'cubed is', user_cubed, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+user_addition = user_num + user_num2
+user_multiply = user_num * user_num2
+
+print (user_num, '+', user_num2, 'is', user_addition)
+print (user_num, '*', user_num2, 'is', user_multiply)
